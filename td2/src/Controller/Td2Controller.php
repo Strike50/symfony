6640bfd8297cfee2df050e21756476961f953428
houlgatte louka <lhoulgattebustamante@gmail.com>
2018-02-21 08:07:00 +0100

    Début de la cinquième séance

--- td2/src/Controller/Td2Controller.php
+++ td2/src/Controller/Td2Controller.php
@@ -2,12 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\ContactRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class Td2Controller extends Controller
 {
+    private $contactRepo;
+
+    public function __construct(ContactRepository $contactRepo){
+        $this->contactRepo=$contactRepo;
+    }
+
+    /**
+     * @Route("/contact/new",name="contact_new")
+     */
+    public function contactNew(){
+        return $this->render();
+    }
     /**
      * @Route("/td2", name="td2")
      */
